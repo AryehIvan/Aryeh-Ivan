9796e010ddfeb81c3043ebf0d739c7cfd72a7614
AryehIvan <63415635+AryehIvan@users.noreply.github.com>
2020-04-12 22:35:36 +0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){     
+    $('.sidenav').sidenav(); 
+    $('.single-item').slick({
+        dots:true,
+        infinite: true,
+        arrows:false,
+    }); 
+});
+
+function goBack(){
+    window.history.back();
+}
+
+mybutton = document.getElementById("myBtn");
+window.onscroll = function(){
+    scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop> 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+let i = 0;
+let txt = 'Aryeh Ivan';
+let speed = 150;
+function typeWriter(){
+    if(i < txt.length){
+        document.getElementById('naming').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter,speed)
+    }
+}
+
+typeWriter();
